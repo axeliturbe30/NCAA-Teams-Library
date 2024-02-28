d411ca709f57ae2a42a197d618f3cc09cda0cabe
axeliturbe30 <146850922+axeliturbe30@users.noreply.github.com>
2024-02-28 10:59:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -21,4 +21,11 @@ function getColumn(url, columnNumber){
          return column;
       }
 
-      var url = ""
+
+var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/main/Sports/NCAA%20Division%20I%20Teams.csv"
+      var university = getColumn(url, 1)
+      var nickname = getColumn(url, 2)
+      var city = getColumn(url, 3)
+      var state = getColumn(url, 4)
+      var enrollment = getColumn(url, 5)
+      var conference = getColumn(url, 6)
